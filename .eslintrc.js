3be92aa972d23165d937c62e0863febefa86e37c
Zerefdev <mosseab.chakrani@gmail.com>
2020-10-08 20:20:37 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'quotes': [
+      2,
+      'single',
+      {
+        'avoidEscape': true
+      }
+    ]
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Vue 3 Typescript</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    console.log('App setup...');
+  }
+});
+</script>
+
+<style lang="less">
+</style>
